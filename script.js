fa7f341c64dea53acda111627fef54bc39494409
seriouslyvictor <gabriel.alunos@CFP127.ITAPEVI>
2024-09-21 15:38:02 -0300

    Adicionado função para dividir sessões dos exercicios

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const allExercises = document.querySelectorAll('.ex--title');
+const adicionarDivisor = function (el) {
+    console.log(el)
+    let section =  el.closest("section");
+    let article = el.closest("article")
+    let lastArticle = section.querySelector(`article:last-child`)
+    if (article === lastArticle) {
+        return true
+    }
+    return false
+}
 
 const gerarNavBar = function (array) {
+    if (array.length <= 0) return;
     const nav = document.querySelector(".sidebar")
     array.forEach(item => {
         console.log(item.closest("article"))
@@ -10,7 +21,12 @@ const gerarNavBar = function (array) {
         navItem.textContent = navItemTitle;
         navItem.setAttribute("href", navLink)
         nav.append(navItem)
+        if (adicionarDivisor(item)) {
+            const hr = document.createElement("hr")
+            nav.append(hr)
+        }
     });
 }
+
 
 gerarNavBar(allExercises)
