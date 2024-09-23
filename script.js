52727e532969882bc09b872d0e9577c1b090b58e
seriouslyvictor <schiavovictor@outlook.com>
2024-09-23 13:34:53 -0300

    Transformado exercicios em Carrossel para ocupar menos espaço na página

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const allExercises = document.querySelectorAll('.ex--title');
 const adicionarDivisor = function (el) {
     console.log(el)
-    let section =  el.closest("section");
+    let section = el.closest("section");
     let article = el.closest("article")
     let lastArticle = section.querySelector(`article:last-child`)
     if (article === lastArticle) {
@@ -28,5 +28,26 @@ const gerarNavBar = function (array) {
     });
 }
 
+const iniciarSliders = function () {
+    const slider1 = document.querySelector(".slider");
+    const params = {
+        cellAlign: 'left',
+        contain: true,
+        prevNextButtons: false,
+        cellSelector: 'article'
+    }
+    const flicky1 = new Flickity(slider1, params)
+    const prevButton = document.querySelector('.prev-button');
+    const nextButton = document.querySelector('.next-button');
+    prevButton.addEventListener('click', function () {
+        flicky1.previous();  // Go to the previous slide
+    });
+    nextButton.addEventListener('click', function () {
+        flicky1.next();  // Go to the next slide
+    });
 
+}
+
+
+iniciarSliders()
 gerarNavBar(allExercises)
